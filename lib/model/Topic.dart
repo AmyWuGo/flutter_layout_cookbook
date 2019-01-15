fea0c5aee183846fbe2caf9b5fb151ec53db5a9e
Reiko Natsume <you@example.com>
2019-01-15 15:29:29 +0900

    first commit

---
+++ lib/model/Topic.dart
@@ -0,0 +1,9 @@
+class Topic {
+  final int type;
+  final String title;
+  final String subtitle;
+  final String imgUrl;
+  final String detailInfo;
+
+  Topic(this.type, this.title, this.subtitle, this.imgUrl, this.detailInfo);
+}
