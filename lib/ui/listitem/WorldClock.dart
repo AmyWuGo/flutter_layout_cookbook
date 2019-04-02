d37cfafa1e71fd7d6623a38db793307cdbf0730f
amywu <geishawu@gamil.com>
2019-04-02 23:28:32 +0900

    added Views.

--- lib/ui/listitem/WorldClock.dart
+++ lib/ui/listitem/WorldClock.dart
@@ -47,35 +47,58 @@ class _MyHomePageState extends State<MyWorldClock> {
 
   _getCardItem({int position = 0}) {
     return InkWell(
-        onTap: () {
-        },
+        onTap: () {},
         child: Container(
             margin: const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 10.0),
             decoration: BoxDecoration(
                 borderRadius: new BorderRadius.circular(8.0),
                 border: Border.all(color: const Color(0xff58B7EA), width: 4.0)),
-            padding: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 16.0),
+            padding: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 0.0),
             child: _getChild(position)));
   }
 
   _getChild(int position) {
+    return Column(
+      children: [
+        _getTitle(position),
+        _getTime(),
+      ],
+    );
+  }
+
+  _getTitle(int position) {
     return ListTile(
-      title: Text(
-          (position % 2 == 0? "TOKYO":"CHINA"),
+      title: Text((position % 2 == 0 ? "TOKYO" : "CHINA"),
           maxLines: 1,
           style: TextStyle(
-              fontSize: 36.0,
+              fontSize: 32.0,
               color: const Color(0xff58B7EA),
               fontWeight: FontWeight.bold)),
       trailing: _getNum(position),
     );
   }
 
-  _getNum(int position) {
-    return  Text(position.toString(),
+  _getTime() {
+    return Container(
+        margin: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 0.0),
+
+        alignment: Alignment.center,
+        padding: const EdgeInsets.fromLTRB(0.0, 32.0, 0.0, 32.0),
+        color: const Color(0xff58B7EA),
+
+
+        child: Text("10:10",
             style: TextStyle(
                 fontSize: 36.0,
-                color: const Color(0xff58B7EA),
-                fontWeight: FontWeight.bold));
+                color: const Color(0xffF7F7F7),
+                fontWeight: FontWeight.bold)));
+  }
+
+  _getNum(int position) {
+    return Text(position.toString(),
+        style: TextStyle(
+            fontSize: 32.0,
+            color: const Color(0xff58B7EA),
+            fontWeight: FontWeight.bold));
   }
 }
