0abc8a98927b7f42901520298ea8bec282ea7440
Reiko Natsume <you@example.com>
2019-04-02 13:05:54 +0900

    added views.

--- lib/ui/listitem/CardViewItem1.dart
+++ lib/ui/listitem/CardViewItem1.dart
@@ -5,8 +5,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_layout_ledger/model/ViewType.dart';
 
 class CardViewItem1 extends StatelessWidget {
-//bottom navigation bar
-
   static Future launcher(BuildContext context) {
     return Navigator.push(
       context,
@@ -35,13 +33,11 @@ class MyCardViewItem1 extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyCardViewItem1> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        itemCount: 100,
+        itemCount: 102,
         itemBuilder: (context, position) {
           return _getCardItem(position: position);
         },
@@ -50,11 +46,45 @@ class _MyHomePageState extends State<MyCardViewItem1> {
   }
 
   _getCardItem({int position = 0}) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(position.toString(), style: TextStyle(fontSize: 22.0),),
-      ),
-    )
+    return Container(
+        margin: const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 10.0),
+        decoration: BoxDecoration(
+            borderRadius: new BorderRadius.circular(8.0),
+            border: Border.all(color: const Color(0xff76C7F2), width: 3.0)),
+        padding: const EdgeInsets.fromLTRB(4.0, 16.0, 4.0, 16.0),
+        child: _getChild(position));
+  }
+
+  _getChild(int position) {
+    return ListTile(
+      title: Text(
+          "Flutter is an open-source mobile application development framework.",
+          style: TextStyle(
+              fontSize: 20.0,
+              color: const Color(0xff625E5E),
+              fontWeight: FontWeight.bold)),
+      leading: _getNum(position),
+    );
+  }
+
+  _getNum(int position) {
+    return Container(
+        alignment: Alignment.center,
+        width: 62.0,
+        height: 60.0,
+        decoration: BoxDecoration(
+          borderRadius: new BorderRadius.only(
+            topLeft: new Radius.circular(4.0),
+            topRight: new Radius.circular(24.0),
+            bottomRight: new Radius.circular(16.0),
+            bottomLeft: new Radius.circular(24.0),
+          ),
+          color: const Color(0xff58B7EA),
+        ),
+        child: Text(position.toString(),
+            style: TextStyle(
+                fontSize: 20.0,
+                color: const Color(0xffF7F7F7),
+                fontWeight: FontWeight.bold)));
   }
 }
